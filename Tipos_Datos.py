5c409f81be91a13b6395bde853dc43276aeca01c
Henry-Ati <74988122+Henry-Ati@users.noreply.github.com>
2021-02-08 23:10:23 -0500

    Add files via upload

---
+++ Tipos_Datos.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 23:09:43 2021
+
+@author: DAVID
+"""
+
+variable=type(8)
+print(variable,"\n"*2)
+print(type(8.5))
+print(type(8))
+print(type(False))
+nota1=45
+promedio=nota1+5
+print(promedio)
