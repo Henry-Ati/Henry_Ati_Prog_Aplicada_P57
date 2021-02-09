c827631bf6f08a7a78100a9536aff2980c480896
Henry-Ati <74988122+Henry-Ati@users.noreply.github.com>
2021-02-08 23:26:19 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 22 14:22:08 2020
+
+@author: DAVID
+"""
+nombre=input("Ingrese su nombre"'\n')
+apellido=input("Ingrese su apellido"'\n')
+localidad=input("Ingrese su localidad"'\n')
+edad=input("Ingrese su edad"'\n')
+space=""
+print("Hola",nombre,space,apellido,space,"Vives en",localidad,space,"tienes",edad,space,"años")
+
